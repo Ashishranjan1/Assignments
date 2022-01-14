349045b3790338dc0d678eeb5a0a0310c80a29a3
Ashish Ranjan <VE00YM333@YMI.COM>
2022-01-14 22:01:29 +0530

    day2 q4 5

---
+++ Assignment_day2/src/day2_q5/SportCar.java
@@ -0,0 +1,13 @@
+package day2_q5;
+
+
+class SportsCar extends Car {
+
+	public SportsCar(int noOfWheel, int noOfPassenger, int model, String make, int noOfDoor) {
+
+		super(noOfWheel, noOfPassenger, model, make, 2);
+	}
+}
+	
+	
+	
